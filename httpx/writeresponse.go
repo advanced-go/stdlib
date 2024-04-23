e088f52ba0124dd9d780568b0f2ce42fb76bdfbf
advanced-go <mark.wilson.go@outlook.com>
2024-04-23 07:22:33 -0500

    Initial commit

--- httpx/writeresponse.go
+++ httpx/writeresponse.go
@@ -105,25 +105,3 @@ func writeStatusContent[E core.ErrorHandler](w http.ResponseWriter, status core.
 
 
 */
-
-// RequestId - return a request id from any type and will create a new one if not found
-func RequestId(t any) string {
-	if t == nil {
-		//s, _ := uuid.NewUUID()
-		return "" // s.String()
-	}
-	requestId := ""
-	switch ptr := t.(type) {
-	case string:
-		requestId = ptr
-	case *http.Request:
-		requestId = ptr.Header.Get(XRequestId)
-	case http.Header:
-		requestId = ptr.Get(XRequestId)
-	}
-	//if len(requestId) == 0 {
-	//	s, _ := uuid.NewUUID()
-	//	requestId = s.String()
-	//}
-	return requestId
-}
