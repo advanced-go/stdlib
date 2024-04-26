7171f845b2d74cd04e7c8a56ca1950ec09e95379
advanced-go <mark.wilson.go@outlook.com>
2024-04-26 08:51:07 -0500

    Update http.go

--- core/http.go
+++ core/http.go
@@ -2,4 +2,9 @@ package core
 
 import "net/http"
 
+const (
+	HealthLivenessPath  = "/health/liveness"
+	HealthReadinessPath = "/health/rediness"
+)
+
 type HttpHandler func(w http.ResponseWriter, r *http.Request)
