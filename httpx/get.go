20c025d3af3de6b06e79dc23cdababed32ef8deb
advanced-go <mark.wilson.go@outlook.com>
2024-05-15 13:35:10 -0500

    Adding Get with Exchange

--- httpx/get.go
+++ httpx/get.go
@@ -29,3 +29,26 @@ func Get(ctx context.Context, uri string, h http.Header) (resp *http.Response, s
 	}
 	return
 }
+
+// GetExchange - process an HTTP Get using an exchange if available
+func GetExchange(ctx context.Context, uri string, h http.Header) (resp *http.Response, status *core.Status) {
+	if len(uri) == 0 {
+		return serverErrorResponse(), core.NewStatusError(http.StatusBadRequest, errors.New("error: URI is empty"))
+	}
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, nil)
+	if err != nil {
+		return serverErrorResponse(), core.NewStatusError(http.StatusBadRequest, err)
+	}
+	if h != nil {
+		req.Header = h
+	}
+	// exchange.Do() will always return a non nil *http.Response
+	resp, status = DoExchange(req)
+	if !status.OK() {
+		status.AddLocation()
+	}
+	return
+}
