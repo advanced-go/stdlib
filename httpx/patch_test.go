e604a56ae522662a522bb3cd8af825499ec63642
advanced-go <mark.wilson.go@outlook.com>
2024-05-23 18:07:52 -0500

    Adding patch type

---
+++ httpx/patch_test.go
@@ -0,0 +1,16 @@
+package httpx
+
+import "fmt"
+
+func ExamplePatch() {
+	patch := Patch{Updates: []Operation{
+		{Op: OpAdd},
+	},
+	}
+
+	fmt.Printf("test: Patch() -> [patch:%v]\n", patch)
+
+	//Output:
+	//test: Patch() -> [patch:{[{add  <nil>}]}]
+
+}
