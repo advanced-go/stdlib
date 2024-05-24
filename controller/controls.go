5f064a93f0b8504680f51d8ad2293659bb254b74
advanced-go <mark.wilson.go@outlook.com>
2024-05-24 05:07:13 -0500

    Adding controller register options

--- controller/controls.go
+++ controller/controls.go
@@ -13,24 +13,8 @@ var (
 	ctrlMap = NewControls()
 )
 
-// RegisterController - add a controller for an egress route
-func RegisterController(ctrl *Controller) error {
-	if ctrl == nil {
-		return errors.New(fmt.Sprintf("invalid argument: Controller is nil"))
-	}
-	if ctrl.Router == nil {
-		return errors.New(fmt.Sprintf("invalid argument: Controller router is nil"))
-	}
-	if ctrl.Router.Primary == nil {
-		return errors.New(fmt.Sprintf("invalid argument: Controller router primary resource is nil"))
-	}
-	if len(ctrl.Router.Primary.Authority) == 0 {
-		if ctrl.Router.Primary.Host == "" {
-			return errors.New(fmt.Sprintf("invalid argument: Controller router primary resource host is empty"))
-		}
-		return ctrlMap.register(ctrl)
-	}
-	return ctrlMap.registerWithAuthority(ctrl)
+func LookupWithAuthority(authority string) (ctrl *Controller, status *core.Status) {
+	return ctrlMap.lookup(authority)
 }
 
 func Lookup(req *http.Request) (ctrl *Controller, status *core.Status) {
@@ -95,22 +79,6 @@ func (p *controls) registerWithAuthority(ctrl *Controller) error {
 	return nil
 }
 
-// Lookup - get a Controller using a URI as the key
-/*
-func (p *controls) lookup(uri string) (*Controller, *core.Status) {
-	if uri == "" {
-		return nil, core.NewStatusError(core.StatusInvalidArgument, errors.New("invalid argument: uri is empty"))
-	}
-	parsed := uri2.Uproot(uri)
-	if !parsed.Valid {
-		return nil, core.NewStatusError(core.StatusInvalidArgument, parsed.Err)
-	}
-	return p.lookupByAuthority(parsed.Authority)
-}
-
-
-*/
-
 // Lookup - get a Controller using an authority
 func (p *controls) lookup(authority string) (*Controller, *core.Status) {
 	if authority == "" {
@@ -124,4 +92,8 @@ func (p *controls) lookup(authority string) (*Controller, *core.Status) {
 		return ctrl, core.StatusOK()
 	}
 	return nil, core.NewStatus(core.StatusInvalidContent)
+}
+
+func (p *controls) remove(key string) {
+	p.m.Delete(key)
 }
--- controller/controls_test.go
+++ controller/controls_test.go
@@ -5,42 +5,6 @@ import (
 	"time"
 )
 
-func ExampleRegisterController() {
-	err := RegisterController(nil)
-	fmt.Printf("test: RegisterController(nil) -> [err:%v]\n", err)
-
-	ctrl := NewController("test-route", nil, nil)
-	ctrl.Router = nil
-	err = RegisterController(ctrl)
-	fmt.Printf("test: RegisterController(ctrl) -> [err:%v]\n", err)
-
-	ctrl = NewController("test-route", nil, nil)
-	err = RegisterController(ctrl)
-	fmt.Printf("test: RegisterController(ctrl) -> [err:%v]\n", err)
-
-	ctrl = NewController("test-route", NewPrimaryResource("", "", 0, "", nil), nil)
-	err = RegisterController(ctrl)
-	fmt.Printf("test: RegisterController(ctrl) -> [err:%v]\n", err)
-
-	ctrl = NewController("test-route", NewPrimaryResource("localhost:8080", "", 0, "", nil), nil)
-	err = RegisterController(ctrl)
-	fmt.Printf("test: RegisterController(ctrl) -> [err:%v]\n", err)
-
-	//err = p.registerWithAuthority(path, ctrl)
-	//fmt.Printf("test: Register(%v) -> [err:%v]\n", path, err)
-	//path = "http://localhost:8080/github/advanced-go/example-domain/activity"
-	//err = p.register(ctrl)
-	//fmt.Printf("test: Register(%v) -> [err:%v]\n", path, err)
-
-	//Output:
-	//test: RegisterController(nil) -> [err:invalid argument: Controller is nil]
-	//test: RegisterController(ctrl) -> [err:invalid argument: Controller router is nil]
-	//test: RegisterController(ctrl) -> [err:invalid argument: Controller router primary resource is nil]
-	//test: RegisterController(ctrl) -> [err:invalid argument: Controller router primary resource host is empty]
-	//test: RegisterController(ctrl) -> [err:<nil>]
-
-}
-
 func ExampleControlsLookup() {
 	p := NewControls()
 	//path := "http://localhost:8080/github/advanced-go/search:google?q=golang"
