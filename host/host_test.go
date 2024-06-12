188a62ee483e10584836cc840b32edb5005439a5
advanced-go <mark.wilson.go@outlook.com>
2024-06-12 08:36:59 -0500

    Testing

--- host/host_test.go
+++ host/host_test.go
@@ -1,1 +1,22 @@
 package host
+
+import (
+	"fmt"
+	"github.com/advanced-go/stdlib/core"
+	"net/http"
+	"net/http/httptest"
+)
+
+func ExampleHost() {
+	r := httptest.NewRecorder()
+	req, _ := http.NewRequest("", "http://localhost:8081/github/advanced-go/search:google?q=golang", nil)
+
+	hostExchange[core.Output](r, req, 0, func(r *http.Request) (*http.Response, *core.Status) {
+		return &http.Response{StatusCode: http.StatusOK}, core.StatusOK()
+	})
+
+	fmt.Printf("test: hostExchange() -> [resp:%v]\n", r.Result().StatusCode)
+
+	//Output:
+	//fail
+}
