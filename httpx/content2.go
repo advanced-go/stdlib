1d470f42a129dd77ab6bc83b8cefeabd342e88f5
advanced-go <mark.wilson.go@outlook.com>
2024-08-30 11:47:14 -0500

    Update content2.go

--- httpx/content2.go
+++ httpx/content2.go
@@ -12,18 +12,26 @@ func Content[T any](body io.Reader) (t T, status *core.Status) {
 	if body == nil {
 		return t, core.NewStatusError(core.StatusInvalidArgument, errors.New("error: body is nil"))
 	}
-	var buf []byte
-	buf, status = io2.ReadAll(body, nil)
-	if !status.OK() {
-		return
-	}
-	if len(buf) == 0 {
-		return t, core.StatusNotFound()
-	}
 	switch p := any(&t).(type) {
 	case *[]byte:
+		var buf []byte
+		buf, status = io2.ReadAll(body, nil)
+		if !status.OK() {
+			return
+		}
+		if len(buf) == 0 {
+			return t, core.StatusNotFound()
+		}
 		*p = buf
 	case *string:
+		var buf []byte
+		buf, status = io2.ReadAll(body, nil)
+		if !status.OK() {
+			return
+		}
+		if len(buf) == 0 {
+			return t, core.StatusNotFound()
+		}
 		*p = string(buf)
 	default:
 		err := json.NewDecoder(body).Decode(p)
