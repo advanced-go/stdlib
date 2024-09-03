667d918d98ad9d71a0a32cb7494e5184b3bd7afe
advanced-go <mark.wilson.go@outlook.com>
2024-09-03 10:39:39 -0500

    Refactoring

--- uri/resolver.go
+++ uri/resolver.go
@@ -8,24 +8,8 @@ import (
 	"strings"
 )
 
-const (
-	proxyKey = "proxy-key"
-)
-
-/*
-type HostEntry struct {
-	Key   string `json:"key"`
-	Host  string `json:"host"`
-	Proxy bool   `json:"proxy"`
-}
-
-*/
-
 type Resolver struct {
 	defaultHost string
-	//m           *sync.Map
-	//proxy       HostEntry
-	//entryFn     func(host string, m *sync.Map) (HostEntry, bool)
 }
 
 func NewResolver(defaultHost string) *Resolver {
@@ -33,68 +17,6 @@ func NewResolver(defaultHost string) *Resolver {
 	r.defaultHost = defaultHost
 	return r
 }
-
-/*
-func NewResolver3(entries []HostEntry) *Resolver {
-	r := new(Resolver)
-	r.m = new(sync.Map)
-	for _, e := range entries {
-		r.m.Store(e.Key, e)
-		if e.Key == proxyKey {
-			r.proxy = e
-			r.proxy.Proxy = false
-		}
-	}
-	r.entryFn = func(host string, m *sync.Map) (HostEntry, bool) {
-		e, ok := load(host, m)
-		if !ok {
-			return e, ok
-		}
-		if !e.Proxy {
-			return e, ok
-		}
-		if r.proxy.Host != "" {
-			return r.proxy, true
-		}
-		return HostEntry{}, false
-	}
-	return r
-}
-*/
-
-/*
-func (r *Resolver) Override(entries []HostEntry) *Resolver {
-	r2 := NewResolver(entries)
-	r2.entryFn = func(host string, m *sync.Map) (HostEntry, bool) {
-		e, ok := load(host, m)
-		if !ok {
-			return r.entryFn(host, r.m)
-		}
-		if !e.Proxy {
-			return e, ok
-		}
-		if r2.proxy.Host != "" {
-			return r2.proxy, true
-		}
-		if r.proxy.Host != "" {
-			return r.proxy, true
-		}
-		return HostEntry{}, false
-	}
-	return r2
-}
-func (r *Resolver) Host(host string) string {
-	if host == "" {
-		return "error: host is empty"
-	}
-	e, ok := r.entryFn(host, r.m)
-	if ok {
-		return e.Host
-	}
-	return host
-}
-
-*/
 
 func (r *Resolver) Url(host, path string, query any, h http.Header) string {
 	path1 := BuildPath(path, query)
@@ -187,63 +109,3 @@ func formatVersion2(version string) string {
 	}
 	return version + "/"
 }
-
-/*
-func entry(host string, m *sync.Map) (HostEntry, bool) {
-	e, ok := load(host, m)
-	if !ok {
-		return e, ok
-	}
-	return e, true
-}
-
-
-*/
-
-/*
-func load(host string, m *sync.Map) (HostEntry, bool) {
-	if m == nil {
-		return HostEntry{}, false
-	}
-	value, ok := m.Load(host)
-	if !ok {
-		return HostEntry{}, false
-	}
-	if e, ok1 := value.(HostEntry); ok1 {
-		return e, true
-	}
-	return HostEntry{}, false
-}
-
-
-*/
-/*
-newUrl := strings.Builder{}
-if host != "" {
-scheme := HttpsScheme
-if strings.Contains(host, Localhost) {
-scheme = HttpScheme
-}
-newUrl.WriteString(scheme)
-newUrl.WriteString("://")
-newUrl.WriteString(host)
-}
-newUrl.WriteString(fmt.Sprintf(path, formatVersion(version)))
-newUrl.WriteString(formatValues(values))
-return newUrl.String()
-newUrl := strings.Builder{}
-	if host != "" {
-		scheme := httpsScheme
-		if strings.Contains(host, localHost) {
-			scheme = httpScheme
-		}
-		newUrl.WriteString(scheme)
-		newUrl.WriteString("://")
-		newUrl.WriteString(host)
-	}
-	newUrl.WriteString(authority)
-	newUrl.WriteString(":")
-	newUrl.WriteString(path)
-	newUrl.WriteString(formatValues(values))
-	return newUrl.String()
-*/
