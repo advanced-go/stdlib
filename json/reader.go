c43db33576ea5a404dbb3dc2ad3377e1c25ef78f
advanced-go <mark.wilson.go@outlook.com>
2024-09-25 10:37:00 -0500

    Updated empty read closer

--- json/reader.go
+++ json/reader.go
@@ -9,7 +9,7 @@ import (
 // NewReadCloser - create an io.ReadCloser from a type
 func NewReadCloser(v any) (io.ReadCloser, int64, *core.Status) {
 	if v == nil {
-		return io.NopCloser(bytes.NewReader([]byte(""))), 0, core.StatusOK()
+		return io.NopCloser(bytes.NewReader([]byte{})), 0, core.StatusOK()
 	}
 	buf, status := Marshal(v)
 	if !status.OK() {
